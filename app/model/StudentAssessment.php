1ca9a867e97ce42ebd9646a1d9d03338efe88877
Pepa Kudlacek <joseph.kudlacek@gmail.com>
2020-10-28 16:24:12 +0100

    Clean code - student asessments by params.

--- app/model/StudentAssessment.php
+++ app/model/StudentAssessment.php
@@ -14,34 +14,6 @@ class StudentAssessment
         $this->database = $database;
     }
 
-    public function getHomeworkAssessments($SemesterId)
-    {
-        return $this->database->query('
-            SELECT class.Id AS ClassId, user.Id AS StudentId, class.Name AS ClassName,
-            semesterassessment.Code AS HomeworkCode,assessment.Name AS AssessmentName, mark.Value AS MarkValue,
-            studentassessment.`Comment` AS AssessmentComment, studentassessment.Date AS AssessmentDate,
-            studentassessment.ResultPoints
-            FROM studentassessment
-            INNER JOIN student
-            ON studentassessment.StudentUserId = student.UserId
-            AND studentassessment.StudentClassId = student.ClassId
-            INNER JOIN user
-            ON user.Id = student.UserId
-            INNER JOIN class
-            ON class.Id = student.ClassId
-            INNER JOIN assessment
-            ON assessment.Id = studentassessment.AssessmentId
-            INNER JOIN semesterassessment
-            ON assessment.Id = semesterassessment.AssessmentId
-            AND semesterassessment.SemesterId = class.SemesterId
-            INNER JOIN mark
-            ON mark.Id = studentassessment.MarkId
-            INNER JOIN homework
-            ON homework.AssessmentId = assessment.Id
-            WHERE class.SemesterId = ?;',
-            $SemesterId);
-    }
-
     public function getStudentAssessments($StudentId, $ClassId)
     {
         return $this->database->query('
@@ -72,62 +44,33 @@ class StudentAssessment
 
     public function getStudentAssessmentsByClass($ClassId)
     {
-        return $this->database->query('
-            SELECT user.Id, user.Name AS StudentName, student.HouseId AS HouseId,
-            MIN(IF(semesterassessment.Code = "ZS", mark.Shortcut, NULL)) AS ZS,
-            MIN(IF(semesterassessment.Code = "ZS", mark.Id, NULL)) AS ZSid,
-            MIN(IF(semesterassessment.Code = "ZS2", mark.Shortcut, NULL)) AS ZS2,
-            MIN(IF(semesterassessment.Code = "ZS2", mark.Id, NULL)) AS ZS2id,
-            MIN(IF(semesterassessment.Code = "Esej", mark.Shortcut, NULL)) AS Esej,
-            MIN(IF(semesterassessment.Code = "Esej", mark.Id, NULL)) AS Esejid,
-            MAX(IF(semesterassessment.Code = "1.PDÚ", mark.Shortcut, NULL)) AS PDU1,
-            MAX(IF(semesterassessment.Code = "1.PDÚ", mark.Id, NULL)) AS PDU1id,
-            MAX(IF(semesterassessment.Code = "2.PDÚ", mark.Shortcut, NULL)) AS PDU2,
-            MAX(IF(semesterassessment.Code = "2.PDÚ", mark.Id, NULL)) AS PDU2id,
-            MAX(IF(semesterassessment.Code = "1.NDÚ", mark.Shortcut, NULL)) AS NDU1,
-            MAX(IF(semesterassessment.Code = "1.NDÚ", mark.Id, NULL)) AS NDU1id,
-            MIN(IF(semesterassessment.Code = "2.NDÚ", mark.Shortcut, NULL)) AS NDU2,
-            MAX(IF(semesterassessment.Code = "2.NDÚ", mark.Id, NULL)) AS NDU2id,
-            MIN(IF(semesterassessment.Code = "3.NDÚ", mark.Shortcut, NULL)) AS NDU3,
-            MAX(IF(semesterassessment.Code = "3.NDÚ", mark.Id, NULL)) AS NDU3id,
-            MIN(IF(semesterassessment.Code = "4.NDÚ", mark.Shortcut, NULL)) AS NDU4,
-            MAX(IF(semesterassessment.Code = "4.NDÚ", mark.Id, NULL)) AS NDU4id,
-            MIN(IF(semesterassessment.Code = "5.NDÚ", mark.Shortcut, NULL)) AS NDU5,
-            MAX(IF(semesterassessment.Code = "5.NDÚ", mark.Id, NULL)) AS NDU5id,
-            MIN(IF(semesterassessment.Code = "6.NDÚ", mark.Shortcut, NULL)) AS NDU6,
-            MAX(IF(semesterassessment.Code = "6.NDÚ", mark.Id, NULL)) AS NDU6id,
-            MIN(IF(semesterassessment.Code = "7.NDÚ", mark.Shortcut, NULL)) AS NDU7,
-            MAX(IF(semesterassessment.Code = "7.NDÚ", mark.Id, NULL)) AS NDU7id,
-            MIN(IF(semesterassessment.Code = "8.NDÚ", mark.Shortcut, NULL)) AS NDU8,
-            MAX(IF(semesterassessment.Code = "8.NDÚ", mark.Id, NULL)) AS NDU8id,
-            MIN(IF(semesterassessment.Code = "9.NDÚ", mark.Shortcut, NULL)) AS NDU9,
-            MAX(IF(semesterassessment.Code = "9.NDÚ", mark.Id, NULL)) AS NDU9id,
-            MIN(IF(semesterassessment.Code = "10.NDÚ", mark.Shortcut, NULL)) AS NDU10,
-            MAX(IF(semesterassessment.Code = "10.NDÚ", mark.Id, NULL)) AS NDU10id
-            FROM student
-            INNER JOIN user
-            ON user.Id = student.UserId
-            INNER JOIN class
-            ON class.Id = student.ClassId
-            LEFT JOIN studentassessment
-            ON studentassessment.StudentUserId = student.UserId
-            AND studentassessment.StudentClassId = student.ClassId
-            LEFT JOIN homework
-            ON homework.AssessmentId = studentassessment.AssessmentId
-            LEFT JOIN mark
-            ON mark.Id = studentassessment.MarkId
-            LEFT JOIN assessment
-            ON assessment.Id = studentassessment.AssessmentId
-            INNER JOIN semesterassessment
-            ON assessment.Id = semesterassessment.AssessmentId
-            AND semesterassessment.SemesterId = class.SemesterId
-            WHERE class.Id = ?
-            GROUP BY user.name
-            ORDER BY user.name;',
-            $ClassId);
+        $params = array(
+            ['class.Id' => $ClassId],
+        );
+
+        return $this->getStudentAssessmentsByParams($params);
     }
 
     public function getStudentAssessmentsBySemester($SemesterId)
+    {
+        $params = array(
+            ['class.SemesterId' => $SemesterId],
+        );
+
+        return $this->getStudentAssessmentsByParams($params);
+    }
+
+    public function insertAssessment($values)
+    {
+        return $this->database->table('studentassessment')->insert($values);
+    }
+
+    public function updateAssessment($values)
+    {
+        return $this->database->table('studentassessment')->where('Id', $values['Id'])->update($values);
+    }
+
+    private function getStudentAssessmentsByParams($params)
     {
         return $this->database->query('
             SELECT user.Id, user.Name AS StudentName, class.Name AS ClassName, student.HouseId AS HouseId,
@@ -179,20 +122,9 @@ class StudentAssessment
             ON semesterassessment.AssessmentId = studentassessment.AssessmentId
             AND semesterassessment.ClassId = studentassessment.StudentClassId
             AND semesterassessment.SemesterId = class.SemesterId
-            WHERE class.SemesterId = ?
-            GROUP BY user.name
-            ORDER BY user.name;',
-                $SemesterId);
+            WHERE',
+            $params,
+            'GROUP BY user.name
+            ORDER BY user.name;');
     }
-
-    public function insertAssessment($values)
-    {
-        return $this->database->table('studentassessment')->insert($values);
-    }
-
-    public function updateAssessment($values)
-    {
-        return $this->database->table('studentassessment')->where('Id', $values['Id'])->update($values);
-    }
-
 }
