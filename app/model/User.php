c9fafd8d1fcd0071a00876cc11b3182eceb903cf
Pepa Kudlacek <joseph.kudlacek@gmail.com>
2020-10-28 16:46:51 +0100

    Change order of queries in User.php.

--- app/model/User.php
+++ app/model/User.php
@@ -20,117 +20,6 @@ class User
         $this->database = $database;
     }
 
-    public function insertStudent($values)
-    {
-        $this->insertUser($values);
-        $this->database->query('
-            SELECT @id := 
-            (SELECT Id FROM user WHERE Name = ?);',
-            $values->username);
-
-        return $this->database->query('
-            INSERT IGNORE INTO student (UserId, ClassId, HouseId)
-            VALUES(
-            @id, ?, ?);',
-            $values->class, $values->house);
-    }
-
-    private function insertUser($values)
-    {
-        $this->checkUser($values->username, $values->email);
-
-        $this->database->query('
-            SELECT @email := ?,
-            @password := ?,
-            @roleId := 2;',
-            $values->email, $values->password);
-
-        return $this->database->query('
-            INSERT INTO user (Name, Email, Password, RoleId)
-            VALUES(
-            ?, @email, @password, @roleId)
-            ON DUPLICATE KEY UPDATE
-            Email = @email, Password = @password, RoleId = @roleId;',
-            $values->username);
-    }
-
-    public function GetUserById($userId)
-    {
-        return $this->database->query('
-            SELECT Id, Name, Email, IsActive
-            FROM user
-            WHERE Id = ?;',
-            $userId);
-    }
-
-    public function getUserClasses($userId)
-    {
-        return $this->database->query('
-            SELECT student.ClassId as ClassId, class.Name AS Name,
-            semester.YearFrom, semester.YearTo
-            FROM student
-            INNER JOIN class
-            ON student.ClassId = class.Id
-            INNER JOIN semester
-            ON semester.Id = class.SemesterId
-            WHERE student.UserId = ?
-            ORDER BY class.FirstLesson DESC;',
-                $userId);
-    }
-
-    public function getUserHistory($userId)
-    {
-        return $this->database->query('
-            SELECT semester.YearFrom, semester.YearTo, class.Name AS ClassName,
-            mark.Shortcut, student.CertificateDate
-            FROM student
-            INNER JOIN class
-            ON student.ClassId = class.Id
-            INNER JOIN semester
-            ON class.SemesterId = semester.Id
-            LEFT JOIN mark
-            ON student.Certificate = mark.Id
-            WHERE student.UserId = ?
-            ORDER BY class.FirstLesson;',
-            $userId);
-    }
-
-    public function deleteUser($studentId)
-    {
-        return $this->database->query('
-            UPDATE user
-            SET Password = NULL,
-            Email = NULL,
-            IsActive = 0
-            WHERE Id = ?;',
-                $studentId);
-    }
-
-    public function activeUser($studentId)
-    {
-        return $this->database->query('
-            UPDATE user
-            SET IsActive = 1
-            WHERE Id = ?;',
-            $studentId);
-    }
-
-    private function checkUser($username, $email)
-    {
-        $user = $this->database->query('
-            SELECT Name
-            FROM user
-            WHERE (Name = ?
-            OR Email = ?)
-            AND Password IS NOT NULL;',
-            $username, $email)
-            ->fetch();
-
-        if (!is_null($user)) {
-            throw new Nette\Database\UniqueConstraintViolationException('User exists.');
-        }
-    }
-
     public function getStudentSum($studentId, $classId)
     {
         $this->database->query('
@@ -170,6 +59,117 @@ class User
 	            AND studentassessment.StudentClassId = @InStudentClassId
 	            ) AS markPoints; 
                 ');
+    }
+
+    public function GetUserById($userId)
+    {
+        return $this->database->query('
+            SELECT Id, Name, Email, IsActive
+            FROM user
+            WHERE Id = ?;',
+            $userId);
+    }
+
+    public function getUserClasses($userId)
+    {
+        return $this->database->query('
+            SELECT student.ClassId as ClassId, class.Name AS Name,
+            semester.YearFrom, semester.YearTo
+            FROM student
+            INNER JOIN class
+            ON student.ClassId = class.Id
+            INNER JOIN semester
+            ON semester.Id = class.SemesterId
+            WHERE student.UserId = ?
+            ORDER BY class.FirstLesson DESC;',
+            $userId);
+    }
+
+    public function getUserHistory($userId)
+    {
+        return $this->database->query('
+            SELECT semester.YearFrom, semester.YearTo, class.Name AS ClassName,
+            mark.Shortcut, student.CertificateDate
+            FROM student
+            INNER JOIN class
+            ON student.ClassId = class.Id
+            INNER JOIN semester
+            ON class.SemesterId = semester.Id
+            LEFT JOIN mark
+            ON student.Certificate = mark.Id
+            WHERE student.UserId = ?
+            ORDER BY class.FirstLesson;',
+            $userId);
+    }
+
+    public function insertStudent($values)
+    {
+        $this->insertUser($values);
+        $this->database->query('
+            SELECT @id := 
+            (SELECT Id FROM user WHERE Name = ?);',
+            $values->username);
+
+        return $this->database->query('
+            INSERT IGNORE INTO student (UserId, ClassId, HouseId)
+            VALUES(
+            @id, ?, ?);',
+            $values->class, $values->house);
+    }
+
+    private function insertUser($values)
+    {
+        $this->checkUser($values->username, $values->email);
+
+        $this->database->query('
+            SELECT @email := ?,
+            @password := ?,
+            @roleId := 2;',
+            $values->email, $values->password);
+
+        return $this->database->query('
+            INSERT INTO user (Name, Email, Password, RoleId)
+            VALUES(
+            ?, @email, @password, @roleId)
+            ON DUPLICATE KEY UPDATE
+            Email = @email, Password = @password, RoleId = @roleId;',
+            $values->username);
+    }
+
+    public function activeUser($studentId)
+    {
+        return $this->database->query('
+            UPDATE user
+            SET IsActive = 1
+            WHERE Id = ?;',
+            $studentId);
+    }
+
+    private function checkUser($username, $email)
+    {
+        $user = $this->database->query('
+            SELECT Name
+            FROM user
+            WHERE (Name = ?
+            OR Email = ?)
+            AND Password IS NOT NULL;',
+            $username, $email)
+            ->fetch();
+
+        if (!is_null($user)) {
+            throw new Nette\Database\UniqueConstraintViolationException('User exists.');
+        }
+    }
+
+    public function deleteUser($studentId)
+    {
+        return $this->database->query('
+            UPDATE user
+            SET Password = NULL,
+            Email = NULL,
+            IsActive = 0
+            WHERE Id = ?;',
+            $studentId);
     }
 
 }
