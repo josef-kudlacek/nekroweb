c38b3d64c5c3e6067097b10710c93786fafc10bb
Pepa Kudlacek <joseph.kudlacek@gmail.com>
2020-11-29 13:07:24 +0100

    Upgrade to include-1.99

--- app/Presenters/BasePresenter.php
+++ app/Presenters/BasePresenter.php
@@ -25,7 +25,7 @@ class BasePresenter extends Nette\Application\UI\Presenter
 
     protected function beforeRender()
     {
-        $this->template->include = 'include-1.98';
+        $this->template->include = 'include-1.99';
 
         $this->loadTemplateFilters();
         $this->loadTemplateFunctions();
