52f53f787d459ff6741e3137183ba4167cf86f65
Pepa Kudlacek <joseph.kudlacek@gmail.com>
2020-12-28 15:08:05 +0100

    Change curriculum by logged user data.

--- app/Presenters/CurriculumPresenter.php
+++ app/Presenters/CurriculumPresenter.php
@@ -22,7 +22,18 @@ class CurriculumPresenter extends BasePresenter
 
     public function renderShow()
     {
-        $this->template->lessons = $this->curriculum->getLessons();
-        $this->template->semester = $this->semester->GetActualSemester();
+        $this->template->lessons = $this->isLogged();
+    }
+
+    private function isLogged()
+    {
+        parent::startup();
+        if ($this->getUser()->loggedIn) {
+            $userIdentity = $this->getUser()->getIdentity();
+
+            return $this->curriculum->getLessonsByUserAndSemester($userIdentity->getId(), $userIdentity->semesterFrom, $userIdentity->semesterTo);
+        } else {
+            return $this->curriculum->getLessons();
+        }
     }
 }
--- app/model/Curriculum.php
+++ app/model/Curriculum.php
@@ -16,13 +16,34 @@ class Curriculum
 
     public function getLessons()
     {
-        return $this->database->query('
-            SELECT lesson.Id, year.Number AS YearNumber, year.CodeName, lesson.Number AS LessonNumber, lesson.Name 
-            FROM necromancy.lesson lesson
-            INNER JOIN necromancy.year year
-            ON lesson.Year = year.Id
-            WHERE year.IsActive = 1
-            ORDER BY lesson.Year, lesson.Number;');
+        $params = array(
+            ['year.IsActive' => 1],
+        );
+
+        return $this->getLessonsByParams($params);
+    }
+
+    public function getLessonsByUserAndSemester($userId, $semesterFrom, $semesterTo)
+    {
+        $semesterTo = isset($semesterTo) ? $semesterTo : $semesterFrom;
+
+        $params = array(
+            [$this->database::literal('lesson.Year IN
+                (SELECT year.Id
+                FROM class
+                INNER JOIN necromancy.year year
+                	ON class.YearId = year.Id
+                INNER JOIN student
+                	ON class.Id = student.ClassId
+                INNER JOIN semester
+                	ON class.SemesterId = semester.Id
+                WHERE student.UserId = ?
+                AND semester.YearFrom <= ?
+                AND (semester.YearTo <= ? OR semester.YearTo IS NULL)
+                )', $userId, $semesterFrom, $semesterTo)],
+        );
+
+        return $this->getLessonsByParams($params);
     }
 
     public function getLessonYears()
@@ -30,5 +51,17 @@ class Curriculum
         return $this->database->query('
             SELECT *
             FROM necromancy.year year;');
+    }
+
+    private function getLessonsByParams($params)
+    {
+        return $this->database->query('
+            SELECT lesson.Id, year.Number AS YearNumber, year.CodeName, lesson.Number AS LessonNumber, lesson.Name 
+            FROM necromancy.lesson lesson
+            INNER JOIN necromancy.year year
+            ON lesson.Year = year.Id
+            WHERE',
+            $params,
+            'ORDER BY year.Number, lesson.Number;');
     }
 }
